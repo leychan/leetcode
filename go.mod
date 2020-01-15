3bdb901c374533c0308de53648c838f763e8bbba
chenlei <leilei1512159@gmail.com>
2020-01-15 16:53:59 +0800

    fix bus

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/leychan/leetcode
 
 go 1.13
-
-replace github.com/leychan/leetcode => ../leetcode
--- xuanzhuanshuzu.go
+++ xuanzhuanshuzu.go
@@ -1,11 +1,14 @@
 package leetcode
 
 func Rotate(nums []int, k int) []int {
-	num := nums[len(nums)-1]
-	for i := 1; i < k; i++ {
-		nums = nums[i:]
-		nums = append(nums, num)
-		num = nums[len(nums)-1]
+	num := []int{}
+	length := len(nums)
+	num = append(num, nums[length-1])
+	for i := 0; i < k; i++ {
+		nums = nums[:len(nums)-1]
+		nums = append(num, nums...)
+		num = []int{}
+		num = append(num, nums[length-1])
 	}
 	return nums
 }
