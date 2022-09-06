4a15bf7d4180ef9632ad3fc96705945e254bcc77
lei <sanyue0315@outlook.com>
2022-09-06 10:00:32 +0800

    k个一组翻转链表

--- php/linkedlist/Linkedlist.php
+++ php/linkedlist/Linkedlist.php
@@ -10,5 +10,27 @@ class ListNode {
         $this->val = $val;
         $this->next = $next;
     }
+
+    public static function generate($arr) {
+        if (empty($arr)) {
+            return null;
+        }
+        $head = new self($arr[0]);
+        $cur = $head;
+
+        $len = count($arr);
+        for ($i = 1; $i < $len; $i++) {
+            $cur->next = new self($arr[$i]);
+            $cur = $cur->next;
+        }
+        return $head;
+    }
+
+    public static function print(ListNode $head) {
+        while ($head != null) {
+            echo $head->val, PHP_EOL;
+            $head = $head->next;
+        }
+    }
 }
 
